f46aafdbe645284b1451b8d3242c787550ecd4b6
Ivan Shoshnikov <shoshnikov.ivan@mail.ru>
2019-10-28 01:28:50 +0300

    Correct

--- Lab4_Mish/Lab4_Mish.cpp
+++ Lab4_Mish/Lab4_Mish.cpp
@@ -4,15 +4,45 @@
 
 using namespace std;
 
+bool addressCheck(string adr, int& countBeforeSobaka, int& countAfterSobaka);
+
 int main()
 {
 	setlocale(LC_ALL,"RUS");
-	string name;
-    cout << "Введит имя\n";
-	getline(cin, name);
+	string adress;
+    cout << "Введит адрес\n";
+	getline(cin, adress);
 	bool flag = true;
-	for (int i = 0; i < name.length(); i++)
-		if ((int)name[i] < 48 || (int)name[i] > 57 && (int)name[i] < 64 || (int)name[i] > 90 && (int)name[i] < 97 || (int)name[i] == 95 || (int)name[i] == 45 || (int)name[i] == 46)
-			flag = false;
-	cout << flag;
+	int countB = 0, countA = 0;
+	if (addressCheck(adress, countB, countA))
+	{
+		for (int i = 0; i < countB - 1; i++)
+			if ((int)adress[i] < 48 || (int)adress[i] > 57 && (int)adress[i] < 64 || (int)adress[i] > 90 && (int)adress[i] < 97 || (int)adress[i] == 95 || (int)adress[i] == 45 || (int)adress[i] == 46)
+				flag = false;
+		if (flag)
+			cout << "Корректно\n";
+		else
+			cout << "Не корректно\n";
+	}
+	else
+		cout << "Не является адресом\n";
+	cout << "CountBefore " << countB << " CountAfter " << countA << endl;
+}
+
+bool addressCheck(string adr, int &countBeforeSobaka, int &countAfterSobaka) 
+{
+	bool sobaka = false;
+	for (int i = 0; i < adr.length(); i++) 
+	{
+		if ((int)adr[i] != 64 && sobaka == false)
+			countBeforeSobaka++;
+		else if (sobaka == true)
+			countAfterSobaka++;
+
+		if ((int)adr[i] == 64)
+			sobaka = true;
+	}
+	if (sobaka && countAfterSobaka && countBeforeSobaka)
+		return true;
+	return false;
 }
